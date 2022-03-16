5c786f244c9de1c2bfacf04199d343946b7d5e8a
lananh <lananhlala31@gmail.com>
2022-03-16 14:44:05 +0700

    add environment

--- event_update_listener.py
+++ event_update_listener.py
@@ -8,7 +8,3 @@ def update_issue():
     data = request.get_json()
     print(data)
     return "Received", 201
-
-if __name__ == "__main__":
-    # co ve dang loi khi request den url nay
-    app.run(port=443, debug=True)
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+from event_update_listener import app
+if __name__ == "__main__":
+    app.run(port=443, debug=True)
